7df2bfd028e0ef75f9a7f107b884760e2b1b6318
Henrik <h.barestrand@gmail.com>
2016-07-29 12:17:33 +0200

    Some window size changes and removed console

--- RandomProg/RandomProg/UI_forms/Form1.Designer.cs
+++ RandomProg/RandomProg/UI_forms/Form1.Designer.cs
@@ -35,9 +35,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 224);
+            this.button1.Location = new System.Drawing.Point(12, 69);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 28);
+            this.button1.Size = new System.Drawing.Size(116, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Generate!";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,15 +48,15 @@
             this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(255, 206);
+            this.textBox1.Size = new System.Drawing.Size(254, 51);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(129, 224);
+            this.button2.Location = new System.Drawing.Point(134, 68);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(142, 28);
+            this.button2.Size = new System.Drawing.Size(132, 29);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -66,7 +66,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(279, 264);
+            this.ClientSize = new System.Drawing.Size(278, 108);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);

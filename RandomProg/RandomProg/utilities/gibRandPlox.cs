b179058c0b362ca1cca405b2d58cc47ff39ca629
Nordic-Coding <tobiasforslund@hotmail.com>
2016-07-29 04:04:08 +0200

    changews

--- RandomProg/RandomProg/utilities/gibRandPlox.cs
+++ RandomProg/RandomProg/utilities/gibRandPlox.cs
@@ -4,18 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace RandomProg.utilities
 {
     public class gibRandPlox
     {
+
+        Random rnd;
+
         gibRandPlox()
         {
-
+            rnd = new Random();
         }
 
-        int gibRand()
+        int gibRand(int max)
         {
             //azdasdasd
+            return rnd.Next(1,max);
         }
+
+        string gibText()
+        {
+            //asdasdasda
+            //asdasdasdasd
+            //<asdasdasd
+            //asdasdasd
+        }
+
     }
 }

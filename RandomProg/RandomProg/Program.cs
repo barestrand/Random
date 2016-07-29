9ea3c0b615dc5bddfea8c6602ee14f0a82744aaa
Henrik <h.barestrand@gmail.com>
2016-07-29 04:37:45 +0200

    Initiate form

--- RandomProg/RandomProg/Program.cs
+++ RandomProg/RandomProg/Program.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿using RandomProg.UI_forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace RandomProg
 {
     class Program
     {
+        public static string getPath(string file)
+        {
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, file);
+        }
+
+        [STAThread]
         static void Main(string[] args)
         {
+            Form form = new Form1();
+
             Console.WriteLine("test5");
             Console.WriteLine("xxxXXXXXXXXXXXXXXXXXDEEERP");
             Console.ReadKey();

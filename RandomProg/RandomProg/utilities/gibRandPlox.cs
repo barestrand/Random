3be4f53fdf6b8826873d02e4efdc7abd0f6ef3ed
Nordic-Coding <tobiasforslund@hotmail.com>
2016-07-29 04:44:19 +0200

    Finished gibText function

--- RandomProg/RandomProg/utilities/gibRandPlox.cs
+++ RandomProg/RandomProg/utilities/gibRandPlox.cs
@@ -70,14 +70,21 @@ namespace RandomProg.utilities
             /*
              * This function constructs a sentence from the adjectives and stuff.
              */
-            
+
             //Generate four random numbers, general sentence form
             /*
              * *Subjective* are *adverb* *verb* *adjective* *subjective*
              */
 
-            
+            int s1, av, v, ad, s2;
 
+            s1 = rnd.Next(0,subjectives.Length-1);
+            s2 = rnd.Next(0, subjectives.Length - 1);
+            av = rnd.Next(0, adverbs.Length - 1);
+            v = rnd.Next(0, verbs.Length - 1);
+            ad = rnd.Next(0, adjectives.Length - 1);
+
+            return subjectives[s1] + "are" + adverbs[av] + verbs[v] + adjectives[ad] + subjectives[s2];
 
         }
 
